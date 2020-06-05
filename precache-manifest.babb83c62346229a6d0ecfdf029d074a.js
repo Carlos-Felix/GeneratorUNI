ce1b0f67c9b4ad0ee2253ce4e3de3a43e3be1b0d
carlos felix <carloncho2357@hotmail.com>
2020-06-04 22:06:54 -0500

    Updates

---
+++ precache-manifest.babb83c62346229a6d0ecfdf029d074a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2551dc19c265a34d74d76e40570bc5",
+    "url": "/GeneratorUNI/index.html"
+  },
+  {
+    "revision": "d64ddc7175b9b1e99576",
+    "url": "/GeneratorUNI/static/css/main.d93595d9.chunk.css"
+  },
+  {
+    "revision": "60c2e1532a95fb3064e0",
+    "url": "/GeneratorUNI/static/js/2.9b3a6e3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GeneratorUNI/static/js/2.9b3a6e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64ddc7175b9b1e99576",
+    "url": "/GeneratorUNI/static/js/main.90604fed.chunk.js"
+  },
+  {
+    "revision": "98f70c3b85bffa7d778a",
+    "url": "/GeneratorUNI/static/js/runtime-main.7085cb5b.js"
+  }
+]);
